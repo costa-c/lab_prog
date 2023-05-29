0234c9da602924514f1fcacb77ea95a4d03e8078
Costa, C.J <ccosta@estgl.ipv.pt>
2023-05-29 14:16:31 +0100

    AdditionQuiz initial commit

---
+++ AdditionQuizApp/src/AdditionQuiz.java
@@ -0,0 +1,37 @@
+public class AdditionQuiz {
+
+    private static int[] firstNumbers; // The first numbers in all ten questions.
+    private static int[] secondNumbers; // The second numbers in all ten questions.
+    private static int[] userAnswers; // The user's answers to the ten questions.
+
+    public static void main(String[] args) throws Exception {
+        System.out.println();
+        System.out.println("Welcome to the addition quiz!");
+        System.out.println();
+        createQuiz();
+        administerQuiz();
+        gradeQuiz();
+    }
+
+    /**
+     * Creates the arrays that hold the numbers for the questions and fills
+     * them with random numbers.
+     */
+    private static void createQuiz() {
+    }
+
+    /**
+     * Asks the user each of the ten quiz questions and gets the user's answers.
+     * The answers are stored in an array, which is created in this subroutine.
+     */
+    private static void administerQuiz() {
+    }
+
+/**
+     * Shows all the questions, with their correct answers, and computes a grade
+     * for the quiz.  For each question, the user is told whether they got
+     * it right.
+     */
+    private static void gradeQuiz() {
+    }
+}
