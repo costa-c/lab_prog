627c039cb0ba861745e99731ef85c08914a027c8
Costa, C.J <ccosta@estgl.ipv.pt>
2023-05-29 14:48:28 +0100

    AdditionQuizNoGlobals complete

--- AdditionQuizApp/src/AdditionQuizNoGlobals.java
+++ AdditionQuizApp/src/AdditionQuizNoGlobals.java
@@ -1,27 +1,24 @@
 import java.util.Scanner;
 
-public class AdditionQuiz {
-
-    private static int[] firstNumbers; // The first numbers in all ten questions.
-    private static int[] secondNumbers; // The second numbers in all ten questions.
-    private static int[] userAnswers; // The user's answers to the ten questions.
+public class AdditionQuizNoGlobals {
 
     public static void main(String[] args) throws Exception {
+        int[] firstNums = new int[10]; // The first numbers in all ten questions.
+        int[] secondNums = new int[10]; // The second numbers in all ten questions.
+        int[] answers = new int[10]; // The user's answers to the ten questions.
         System.out.println();
         System.out.println("Welcome to the addition quiz!");
         System.out.println();
-        createQuiz();
-        administerQuiz();
-        gradeQuiz();
+        createQuiz(firstNums,secondNums);
+        administerQuiz(firstNums,secondNums,answers);
+        gradeQuiz(firstNums,secondNums,answers);
     }
 
     /**
      * Creates the arrays that hold the numbers for the questions and fills
      * them with random numbers.
      */
-    private static void createQuiz() {
-        firstNumbers = new int[10];
-        secondNumbers = new int[10];
+    private static void createQuiz(int[] firstNumbers, int[] secondNumbers) {
         for (int i = 0; i < 10; i++) {
             firstNumbers[i] = (int) (Math.random() * 50 + 1); // in the range 1 to 50
             secondNumbers[i] = (int) (Math.random() * 50); // in the range 0 to 49
@@ -31,10 +28,12 @@ public class AdditionQuiz {
     /**
      * Asks the user each of the ten quiz questions and gets the user's answers.
      * The answers are stored in an array, which is created in this subroutine.
+     * @param userAnswers 
+     * @param secondNumbers
+     * @param firstNumbers
      */
-    private static void administerQuiz() {
+    private static void administerQuiz(int[] firstNumbers, int[] secondNumbers, int[] userAnswers) {
         Scanner stdio = new Scanner(System.in);
-        userAnswers = new int[10];
         for (int i = 0; i < 10; i++) {
             int questionNum = i + 1;
             System.out.printf("Question %2d:  What is %2d + %2d  ? ",
@@ -49,7 +48,7 @@ public class AdditionQuiz {
      * for the quiz. For each question, the user is told whether they got
      * it right.
      */
-    private static void gradeQuiz() {
+    private static void gradeQuiz(int[] firstNumbers, int[] secondNumbers, int[] userAnswers) {
         System.out.println();
         System.out.println("Here are the correct answers:");
         int numberCorrect = 0;
