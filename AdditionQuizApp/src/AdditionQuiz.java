28d7faa29b966ae0a50f974d8a487dba856aea3c
Costa, C.J <ccosta@estgl.ipv.pt>
2023-05-29 14:36:02 +0100

    AdditionQuiz ver1 complete

--- AdditionQuizApp/src/AdditionQuiz.java
+++ AdditionQuizApp/src/AdditionQuiz.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class AdditionQuiz {
 
     private static int[] firstNumbers; // The first numbers in all ten questions.
@@ -18,6 +20,12 @@ public class AdditionQuiz {
      * them with random numbers.
      */
     private static void createQuiz() {
+        firstNumbers = new int[10];
+        secondNumbers = new int[10];
+        for (int i = 0; i < 10; i++) {
+            firstNumbers[i] = (int) (Math.random() * 50 + 1); // in the range 1 to 50
+            secondNumbers[i] = (int) (Math.random() * 50); // in the range 0 to 49
+        }
     }
 
     /**
@@ -25,13 +33,43 @@ public class AdditionQuiz {
      * The answers are stored in an array, which is created in this subroutine.
      */
     private static void administerQuiz() {
+        Scanner stdio = new Scanner(System.in);
+        userAnswers = new int[10];
+        for (int i = 0; i < 10; i++) {
+            int questionNum = i + 1;
+            System.out.printf("Question %2d:  What is %2d + %2d  ? ",
+                    questionNum, firstNumbers[i], secondNumbers[i]);
+            userAnswers[i] = stdio.nextInt();
+        }
+        stdio.close();
     }
 
-/**
+    /**
      * Shows all the questions, with their correct answers, and computes a grade
-     * for the quiz.  For each question, the user is told whether they got
+     * for the quiz. For each question, the user is told whether they got
      * it right.
      */
     private static void gradeQuiz() {
+        System.out.println();
+        System.out.println("Here are the correct answers:");
+        int numberCorrect = 0;
+        int grade;
+        for (int i = 0; i < 10; i++) {
+            int questionNum = i + 1;
+            int correctAnswer = firstNumbers[i] + secondNumbers[i];
+            System.out.printf("   Question %2d:  %2d + %2d  =  %2d.  ",
+                    questionNum, firstNumbers[i], secondNumbers[i], correctAnswer);
+            if (userAnswers[i] == correctAnswer) {
+                System.out.println("You were CORRECT.");
+                numberCorrect++;
+            } else {
+                System.out.println("You said " + userAnswers[i] + ", which is INCORRECT.");
+            }
+        }
+        grade = numberCorrect * 10;
+        System.out.println();
+        System.out.println("You got " + numberCorrect + " questions correct.");
+        System.out.println("Your grade on the quiz is " + grade);
+        System.out.println();
     }
-}
+} // end class AdditionQuiz
